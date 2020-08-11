e6a922e7cd745074ab8f2871286797a792f012c1
JustLiving <JustinTLivingston@gmail.com>
2020-08-10 18:06:19 -0700

    complete objective2lab1

---
+++ labs/Objective2Lab1.java
@@ -0,0 +1,8 @@
+public class Objective2Lab1 {
+  public static void main(String[] args) {
+    String hello = "Hello World";
+    // I could also have broken it up into two lines and make String Hello;
+    // and hello = "Hello World"
+    System.out.println(hello);
+  }
+}
