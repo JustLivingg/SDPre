b8a08864773b37de21bf6a84dcfb4fc7c423987f
JustLiving <JustinTLivingston@gmail.com>
2020-08-11 12:04:50 -0700

    complete objective3lab2

---
+++ labs/Objective3Lab2.java
@@ -0,0 +1,8 @@
+public class Objective3Lab2 {
+  public static void main(String[] args) {
+    double num1 = 10; //10 squared is 100
+    double num2 = 8; // 8 squared is 64
+    double hypotenuse = Math.sqrt(100 + 64);
+    System.out.println("The hypotenuse of a triangle with sides " +  num1 + " and " + num2 + " is " + hypotenuse);
+  }
+}
