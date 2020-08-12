c3df43b596c75a10705b8e2785ae9fb9bdeb9118
JustLiving <JustinTLivingston@gmail.com>
2020-08-12 13:46:44 -0700

    complete objective7lab4

---
+++ labs/Objective7Lab4.java
@@ -0,0 +1,16 @@
+public class Objective7Lab4 {
+  public static void main(String[] args) {
+
+    int counter = 0;
+    int total = 0;
+
+    while(counter <= 20) {
+
+      total = total + counter;
+      counter = counter + 1;
+    }
+
+    System.out.println(total);
+
+  }
+}
